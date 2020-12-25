b78e838750f38fb3c0e7e7ee6a8bea9c54d6d192
SunDreamFly <1819163694@qq.com>
2020-12-25 14:31:41 +0800

    two second time

--- app/src/main/java/com/example/buttonapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/buttonapp/fragment/HomeFragment.java
@@ -3,12 +3,14 @@ package com.example.buttonapp.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.buttonapp.R;
+import com.youth.banner.Banner;
 
 /**
  */
@@ -23,6 +25,15 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        Banner banner = view.findViewById(R.id.banner);
+        RecyclerView lan = view.findViewById(R.id.rv_lan);
+        RecyclerView rvck = view.findViewById(R.id.rv_Ck);
+        RecyclerView food = view.findViewById(R.id.rv_food);
     }
 }
